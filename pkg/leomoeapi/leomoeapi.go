d66236ad66f776115765c248c2f9518a242feee6
tsosunchia <59512455+tsosunchia@users.noreply.github.com>
2023-05-24 19:33:27 +0800

    增加了局域网以及不合法输入判断

--- pkg/leomoeapi/leomoeapi.go
+++ pkg/leomoeapi/leomoeapi.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/websocket"
+	"net"
 	"strings"
 )
 
@@ -60,9 +61,71 @@ func (r Result) String() string {
 	return r.Data
 }
 
-func Find(query string, params ...string) (result fmt.Stringer, err error) {
+func isPrivateOrReserved(ip net.IP) bool {
+	privateIPv4 := []string{
+		"10.0.0.0/8",
+		"172.16.0.0/12",
+		"192.168.0.0/16",
+		"100.64.0.0/10",   // Shared Address Space (also known as Carrier-Grade NAT, or CGN)
+		"198.18.0.0/15",   // Network Interconnect Device Benchmark Testing
+		"198.51.100.0/24", // TEST-NET-2
+		"203.0.113.0/24",  // TEST-NET-3
+		"240.0.0.0/4",     // Reserved for future use
+	}
 
-	//res, err := ipgeo.LeoIP(query)
+	privateIPv6 := []string{
+		"FC00::/7",  // Unique Local Address
+		"FE80::/10", // Link-local address
+	}
+
+	reservedIPv4 := []string{
+		"0.0.0.0/8",
+		"6.0.0.0/7",
+		"11.0.0.0/8",
+		"21.0.0.0/8",
+		"22.0.0.0/8",
+		"26.0.0.0/8",
+		"28.0.0.0/8",
+		"29.0.0.0/8",
+		"30.0.0.0/8",
+		"33.0.0.0/8",
+		"55.0.0.0/8",
+		"214.0.0.0/8",
+		"215.0.0.0/8",
+	}
+
+	reservedIPv6 := []string{
+		"::1/128",  // loopback address
+		"::/128",   // unspecified address
+		"FF00::/8", // multicast address
+	}
+
+	for _, cidr := range append(privateIPv4, reservedIPv4...) {
+		_, network, _ := net.ParseCIDR(cidr)
+		if network.Contains(ip) {
+			return true
+		}
+	}
+
+	if ip.To4() == nil {
+		for _, cidr := range append(privateIPv6, reservedIPv6...) {
+			_, network, _ := net.ParseCIDR(cidr)
+			if network.Contains(ip) {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func Find(query string, params ...string) (result fmt.Stringer, err error) {
+	if net.ParseIP(query) == nil {
+		return Result{""}, nil // 如果 query 不是一个有效的 IP 地址，返回空字符串
+	}
+	if isPrivateOrReserved(net.ParseIP(query)) {
+		return Result{""}, nil // 如果 query 是一个私有或保留地址，返回空字符串
+	}
 	res, err := FetchIPInfo(query)
 	if err != nil {
 		return nil, err
@@ -71,7 +134,7 @@ func Find(query string, params ...string) (result fmt.Stringer, err error) {
 		Data: strings.Join(func() []string {
 			dataSlice := make([]string, 0, 7)
 			fields := []string{
-				res.Asnumber,
+				"AS" + res.Asnumber,
 				res.Country,
 				res.Prov,
 				res.City,
